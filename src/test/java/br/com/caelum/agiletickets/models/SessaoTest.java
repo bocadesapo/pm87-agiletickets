4116471b3650a473450309bac66747fca915d9a1
vivaldo <vivaldopinto@bb.com.br>
2014-12-10 09:11:21 -0200

    Corrigido bug de reserva e melhorado os testes

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -38,4 +38,13 @@ public class SessaoTest {
 		Assert.assertEquals(2, sessao.getIngressosDisponiveis().intValue());
 	}
 	
+	@Test
+	public void reservarIngressosDeveDiminuirTodosIngressosDisponiveis() throws Exception {
+		Sessao sessao = new Sessao();
+		sessao.setTotalIngressos(5);
+
+		sessao.reserva(5);
+		Assert.assertEquals(0, sessao.getIngressosDisponiveis().intValue());
+	}
+	
 }
